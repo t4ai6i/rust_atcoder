878ce3970171c1f7c8028fb3e6ccb65b4f64c3e5
t4ai6i <t4ai6i>
2022-05-21 17:53:35 +0900

    start typical90

---
+++ typical90/src/bin/001.rs
@@ -0,0 +1,38 @@
+fn binary_search(mid: isize, xs: &[isize], n: usize, l: isize, k: isize) -> bool {
+    let mut count = 0;
+    let mut start = 0;
+
+    for i in 0..n {
+        if xs[i] - start >= mid && mid <= l - xs[i] {
+            count += 1;
+            start = xs[i];
+        }
+    }
+
+    if count >= k {
+        true
+    } else {
+        false
+    }
+}
+
+fn main() {
+    proconio::input! {
+        n: usize,
+        l: isize,
+        k: isize,
+        mut xs: [isize; n],
+    }
+    let mut left = -1;
+    let mut right = l + 1;
+    while right - left > 1 {
+        let mid = left + (right - left) / 2;
+        dbg!(&left, &right, &mid);
+        if binary_search(mid, &xs, n, l, k) {
+            left = mid;
+        } else {
+            right = mid;
+        }
+    }
+    println!("{}", left);
+}
