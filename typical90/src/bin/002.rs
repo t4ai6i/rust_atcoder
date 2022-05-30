1d24f1b7b4573f229641366c54058999acbf7da6
t4ai6i <t4ai6i>
2022-05-30 21:12:51 +0900

    refactor typical90 002

--- typical90/src/bin/002.rs
+++ typical90/src/bin/002.rs
@@ -1,19 +1,19 @@
-fn accumulator(xs: &[i32]) -> bool {
-    fn accumulator_inner(prev: i32, xs: &[i32]) -> bool {
-        let prev = match xs.get(0) {
-            None => return if prev == 0 {
+fn accumulator(xs: &[char]) -> bool {
+    fn accumulator_inner(depth: i32, xs: &[char]) -> bool {
+        let depth = match xs.get(0) {
+            None => return if depth == 0 {
                 true
             } else {
                 false
             },
             Some(&x) => {
-                x + prev
+                if x == '(' { depth + 1 } else { depth - 1 }
             }
         };
-        if prev < 0 {
+        if depth < 0 {
             return false;
         }
-        accumulator_inner(prev, &xs[1..])
+        accumulator_inner(depth, &xs[1..])
     }
     accumulator_inner(0, xs)
 }
@@ -23,22 +23,16 @@ fn main() {
         n: usize
     }
     let results = (0..(1 << n)).into_iter().map(|bit| {
-        let mut v = vec![0; n];
+        let mut v = vec!['\0'; n];
         for i in 0..n {
             let bit_i = n - 1 - i;
-            v[bit_i] = if bit & (1 << i) == 0 { 1 } else { -1 };
+            v[bit_i] = if bit & (1 << i) == 0 { '(' } else { ')' };
         }
         v
     }).filter(|xs| {
         accumulator(xs)
     }).map(|xs| {
-        xs.iter().map(|&x| {
-            match x {
-                1 => '(',
-                -1 => ')',
-                n => panic!("Unexpected value = {}", n),
-            }
-        }).collect::<Vec<char>>().iter().collect::<String>()
+        xs.iter().collect::<String>()
     }).collect::<Vec<String>>();
     for result in results {
         println!("{}", result);
